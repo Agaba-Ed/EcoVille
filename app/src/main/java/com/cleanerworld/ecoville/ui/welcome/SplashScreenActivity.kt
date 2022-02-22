e39f36edd175479327136c4ab1ba0884e3d7620e
Suzan-Tulinde <59966248+Agaba-Ed@users.noreply.github.com>
2022-02-22 17:25:02 +0300

    Updated splashcreen

--- app/src/main/java/com/cleanerworld/ecoville/ui/welcome/SplashScreenActivity.kt
+++ app/src/main/java/com/cleanerworld/ecoville/ui/welcome/SplashScreenActivity.kt
@@ -2,10 +2,7 @@ package com.cleanerworld.ecoville.ui.welcome
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import com.cleanerworld.ecoville.R
-import com.cleanerworld.ecoville.databinding.ActivityMainBinding
 import com.cleanerworld.ecoville.databinding.ActivitySplashScreenBinding
 
 
@@ -13,12 +10,18 @@ class SplashScreenActivity : AppCompatActivity(){
     private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivitySplashScreenBinding.inflate(layoutInflater)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.splashScreenButton.setOnClickListener{
-            goToWalkThroughScreen()
-        }
 
+        //wait for 3 seconds and then start the next activity
+        Thread(Runnable {
+            try {
+                Thread.sleep(3000)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+            goToWalkThroughScreen()
+        }).start()
     }
     fun goToWalkThroughScreen(){
         startActivity(Intent(this,WalkThroughActivity::class.java))
